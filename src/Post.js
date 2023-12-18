9b4f771322ba1f5fec3d1941f124af445e8a159c
strelok84 <streloc84@gmail.com>
2023-12-18 17:37:45 +0400

    eslint

---
+++ src/Post.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+
+const node = document.getElementById("root");
+
+class Post extends Component {
+    render() {
+        return React.createElement("div", { className: "post" },
+            React.createElement("h2", { className: "postAuthor", id: this.props.id }, this.props.user, React.createElement("span", { className: "postBody" }, this.props.content))
+        )
+    }
+
+
+}
+
+Post.PropTypes = {
+    user: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -14,4 +14,14 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); */
+
+import React, { Component } from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+
+const node = document.getElementById("root");
+
+const root = React.createElement('div', {}, React.createElement('h1', {}, "Hello world!"));
+
+render(root, node);
